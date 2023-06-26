d3e24fd59a2c548281c5667b7090e389b1866876
root <suwilanjinakamba939@gmail.com>
2023-06-26 00:07:32 -0700

    Added challenge 4 :tada:

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-*  _strlen - returns the length of a string 
+*  _strlen - returns the length of a string
 *  @s: string to check
 *  Return: 0 is success
 */
@@ -8,8 +8,8 @@ int _strlen(char *s)
 {
 	int i = 0;
 
-    for (i = 0; *s++; i++;)
+for (i = 0; *s++; i++)
 {
-	return (a);
+	return (i);
 }
 }
