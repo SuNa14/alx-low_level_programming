2e5569b5557ea2be2f1a405518fd4ccc7d74f415
root <suwilanjinakamba939@gmail.com>
2023-07-05 08:29:15 -0700

    0x08-recursion

---
+++ 0x08-recursion/0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+/**
+ * _pow_recursion - Returns the value of x raised to the power of y.
+ * @x: The base number.
+ * @y: The exponent.
+ *
+ * Return: The value of x raised to the power of y.
+ *         If y is lower than 0, returns -1 to indicate an error.
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+}
