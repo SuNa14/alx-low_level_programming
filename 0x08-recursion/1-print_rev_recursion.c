ad0a3841365d5dadb0d80eaf8e3e0abfbbd7bf20
root <suwilanjinakamba939@gmail.com>
2023-07-05 08:01:27 -0700

    Added challenge 2 :tada:

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -4,12 +4,14 @@
  * @s:string
  * Return:void
  */
-void _puts_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
-if (*s)
+if (*s != 0)
 {
 	_puts_recursion(s +1);
 	_putchar(*s);
 
 }
+else
+	return;
 }
