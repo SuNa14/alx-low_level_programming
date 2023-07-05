99172ae8a65aa807b655635219b944514dff48eb
root <suwilanjinakamba939@gmail.com>
2023-07-05 08:34:22 -0700

    Added challenge 6 :tada:

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * pow_recursion - power 
+ * pow_recursion - power
  * @x: int
  * @y: int
  * Return: int
@@ -11,7 +11,7 @@ if (y < 0)
 {
 return (-1);
 }
-else if(y != 0)
+else if (y != 0)
 return (x * _pow_recursion(x, y - 1));
 else
 return (1);
