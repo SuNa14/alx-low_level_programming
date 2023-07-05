b780efad0b8299b0df9529d56f475d1497158315
root <suwilanjinakamba939@gmail.com>
2023-07-05 07:55:16 -0700

    Added challenge 1 :tada:

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,10 +6,14 @@
  */
 void _puts_recursion(char *s)
 {
-int i = 0;
-while((s[i] = '\0'))
+if (*s)
 {
-_putchar(s[i]);
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
-_putchar('\n');
+else
+{
+	_putchar('\n');
+}
+
 }
