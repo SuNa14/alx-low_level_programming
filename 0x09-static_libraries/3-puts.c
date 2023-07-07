5e1300ea8875770009471e59eb3d468bf3450972
root <suwilanjinakamba939@gmail.com>
2023-07-07 13:32:22 -0700

    Added challenge 38 :tada:

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ * @str: the string
+ *
+ * Return: the length of the string
+ */
+void _puts(char *str)
+{
+	int j = 0;
+
+	while (str[j] != '\0')
+	{
+		_putchar(str[j]);
+		j++;
+	}
+	_putchar('\n');
+}
