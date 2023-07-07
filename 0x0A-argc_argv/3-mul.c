46d061b937b2c7f36a22454b390bcc4829f3016a
root <suwilanjinakamba939@gmail.com>
2023-07-07 14:17:43 -0700

    Added challenge 3 :tada:

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,1 +1,23 @@
-#in
+#include <stdio.h>
+/**
+ * main - multiplies two numbers
+ * @argc: argument count
+ * @argv: arguments
+ * Return: 0
+*/
+int main(int argc, char **argv)
+{
+int a, b;
+
+if (argc < 3)
+{
+printf("Error");
+return (1);
+}
+else
+{
+a = atoi(argv[1]);
+b = atoi(argv[2]);
+printf("%d", a * b);
+}
+}
