bb9a4e1c9c139e6c71814b79d805e686dc91d98b
root <suwilanjinakamba939@gmail.com>
2023-07-07 13:57:29 -0700

    Added challenge 0 :tada:

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints the name of the program
+ * @argc: argument count
+ * @argv: argument array
+ * Return: 0
+*/
+int main(int argc, char **argv)
+{
+(void)argc;
+printf("%s", argv[0]);
+return (0);
+}
