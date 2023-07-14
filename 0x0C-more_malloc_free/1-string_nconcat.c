73b55a2d4e0107b45e23905ff2d2dfd4607706f7
root <suwilanjinakamba939@gmail.com>
2023-07-14 06:32:33 -0700

    Added challenge 4 :tada:

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,8 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+if(strlen(s2) >= n)
+{
+}
---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * array_range - returns a pointer to an array of numbers 
+ * @min: Lower bound
+ * @max: Upper bound
+ * Return: ptr or NULL
+ *
+*/
+int *array_range(int min, int max)
+{
+int *p;
+
+int j, size;
+
+if(min > max)
+{
+return (NULL);
+}
+size = max - min + 1;
+p = malloc(sizeof(int) * size);
+
+if(ptr == NULL)
+{
+return (NULL);
+}
+for(j = 0; min <= max; j++)
+{
+p[j] = min++;
+}
+return (ptr);
+}
